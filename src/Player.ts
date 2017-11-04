390ca2fe210d5b8af535771c99be4676eb279447
Pouya Larjani <plj@fb.com>
2017-11-04 15:23:59 -0700

    Player inventory

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,8 @@
-import { Experienceable, Named, Dict } from './utils';
+import { Experienceable, Named, Dict, Quantity } from './utils';
+import { Item } from './Item';
 import { Recipe } from './Recipe';
 
 export type Player = Named & Experienceable & {
     recipes: Dict<Recipe>;
+    inventory: Quantity<Item>[];    
 }
