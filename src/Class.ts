cd01257daf77334407160913b71a22d3fdff4f91
Pouya Larjani <plj@fb.com>
2017-11-04 15:30:15 -0700

    Fix classes name

--- src/Class.ts
+++ src/Class.ts
@@ -3,4 +3,4 @@ import { Dict, nameify, Named } from "./utils";
 export type Class = Named & {
 }
 
-export const Qualities: Dict<Class> = nameify(require("./data/Classes.json"));
+export const Classes: Dict<Class> = nameify(require("./data/Classes.json"));
