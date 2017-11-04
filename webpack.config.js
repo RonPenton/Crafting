7b5a8571b78ad0c2596e2e1d84e70597dace7f08
Pouya Larjani <plj@fb.com>
2017-11-04 14:27:05 -0700

    Reducing size of generated files

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-var extractPlugin = new ExtractTextPlugin({ filename: 'bundle.css' });
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     context: __dirname,
-    entry: "./src/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
+
+    entry: {
+        'vendor-trash': './src/index.tsx',
     },
 
-    devtool: "source-map",
+    output: {
+        filename: '[name].js',
+        path: __dirname + '/dist',
+    },
+
+    devtool: 'source-map',
+
     devServer: {
         contentBase: './',
-        hot: true
+        hot: true,
     },
 
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json", ".scss"]
+        extensions: ['.ts', '.tsx', '.js', '.json', '.scss'],
     },
 
     module: {
         rules: [{
                 test: /\.tsx?$/,
-                use: ["ts-loader"]
+                use: ['ts-loader'],
             },
 
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
-                    //use: extractPlugin.extract({ // for prod build. Bother.
-                    // use: [
-                    //     'css-loader',
-                    //     {
-                    //         loader: 'sass-loader',
-                    //         options: {
-                    //         }
-                    //     }]
-                    //})
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
             }
         ]
     },
 
     plugins: [
-        extractPlugin,
-        new HtmlWebpackPlugin({ filename: 'index.html', template: 'index.html' }),
+        new ExtractTextPlugin('[name].css'),
+        new HtmlWebpackPlugin({ template: './src/index.ejs' }),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
     ],
+
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'ramda': 'Ramda',
+    },
 };
