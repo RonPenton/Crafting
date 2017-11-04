aeed51d3cd9e08a4a40230c5d1f5f915bc1849af
Pouya Larjani <plj@fb.com>
2017-11-04 16:15:24 -0700

    Adevneturer class type

--- src/Adventurer.ts
+++ src/Adventurer.ts
@@ -1,8 +1,9 @@
+import { Class } from './Class';
 import { Item } from "./Item";
 import { Experienceable, Named, Quantity } from './utils';
 
 export interface Adventurer extends Named, Experienceable {
-    class: string;
+    class: Class;
     personality: string[];
     inventory: Quantity<Item>[];
     encumbrance: number;
